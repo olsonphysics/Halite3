abb0a6e3d2aea16ca38b9006f6ba481e7d45469a
bryanolson <archerluv@yahoo.com>
2018-10-20 00:40:37 -0700

    v4 in the works

--- MyBot.py
+++ MyBot.py
@@ -18,8 +18,13 @@ game = hlt.Game()
 #Add Code Here for anything that will exceed the 2 second per turn timer, such as initial analysis of the map (size, max halite, # of players)#
 
 game.ready("PicoGrande")
-
 logging.info("Successfully created bot! My Player ID is {}.".format(game.my_id))
+
+
+build = True
+min_halite = 100
+#future optimization is to make scope dynamic based on map size and player count#
+scope = 5
 
 """ <<<Game Loop>>> """
 
@@ -36,31 +41,54 @@ while True:
 
     for ship in me.get_ships():
         b_ship = shipclass.BetterShip(ship, game_map)
-        if b_ship.halite_amount > 600:
+
+#returning based commands#
+        if b_ship.returning:
             command_queue.append(
                 ship.move(game_map.naive_navigate(ship, me.shipyard.position)))
+        # elif b_ship.exploring:
+        #     command_queue.append(
+        #         ship.move(game_map.naive_navigate(ship, positionals.Position(0,0))))
+        elif b_ship.halite_amount > 700 and not b_ship.returning:
+            b_ship.returning = True
+            command_queue.append(
+                ship.move(game_map.naive_navigate(ship, me.shipyard.position)))
+
+#shipyard based commands#
         elif b_ship.position == me.shipyard.position and not game_map[b_ship.position.directional_offset(spawn_cardinals[spawn_counter%4])].is_occupied:
+            b_ship.returning = False
             command_queue.append(
                 ship.move(spawn_cardinals[spawn_counter%4]))
             spawn_counter += 1
-        elif game_map[ship.position].halite_amount > 100:
+        elif game_map[ship.position].halite_amount > min_halite:
             command_queue.append((ship.stay_still()))
+            # b_ship.exploring = False
+            # b_ship.target = b_ship.position
+
+#if halite is less than 100, implement scope to find good spot#
+#SCOPE SCOPE SCOPE#
+        elif b_ship.scarce_move(b_ship.check_surroundings(), min_halite) == True:
+            b_ship.exploring = True
+            b_ship.target = b_ship.get_target2(scope)
+            
         elif b_ship.safe_move(b_ship.check_surroundings()):
             command_queue.append(ship.move(game_map.naive_navigate(ship, b_ship.safe_move(b_ship.check_surroundings()))))
 
-
-            
-        # elif game_map[ship.position].halite_amount < constants.MAX_HALITE / 10 or ship.is_full:
-        #     command_queue.append(
-        #         ship.move(
-        #             random.choice([ Direction.North, Direction.South, Direction.East, Direction.West ])))
         else:
             command_queue.append(ship.stay_still())
 
     # If the game is in the first 200 turns and you have enough halite, spawn a ship.
     # Don't spawn a ship if you currently have a ship at port, though - the ships will collide.
-    if game.turn_number <= 200 and me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied:
+
+    if len(game.players) == 2:
+        for player in game.players:
+            if player != game.my_id:
+                if len(game.players[player].get_ships()) <= 1 and game.turn_number >= 100:
+                    build = False
+
+    if game.turn_number <= 200 and me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied and build == True:
         command_queue.append(me.shipyard.spawn())
+
 
     # Send your moves back to the game environment, ending this turn.
     game.end_turn(command_queue)
